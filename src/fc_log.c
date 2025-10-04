d1a8c190b0aa39d72c5ecb0e40312d192e3ce808
独霸一方-_- <59137524+fool-cat@users.noreply.github.com>
2025-10-05 00:53:07 +0800

    perf: ⚡ fc_log优化,提供对接fc_pool的方式,实现高性能"并行"日志

--- src/fc_log.c
+++ src/fc_log.c
@@ -12,25 +12,22 @@
 #include <stdarg.h>
 #include <string.h>
 
+#include "fc_compiler.h"
 #include "fc_config.h"
 #include "fc_log.h"
 #include "fc_port.h"
 
-#ifndef USE_FC_VSNPRINTF
-    #define USE_FC_VSNPRINTF 1 /**< 是否使用fc_vsnprintf进行格式化 */
-#endif
+#ifndef fc_assert
+    #define fc_assert(x) ((void)(0))
+#endif  //\ fc_assert
 
-#if USE_FC_VSNPRINTF
+#if FC_LOG_USE_FC_POOL
     #include "fc_stdio.h"
     #define LOG_VSNPRINTF fc_vsnprintf
 #else
     #include <stdio.h>
     #define LOG_VSNPRINTF vsnprintf
 #endif  //\ LOG_USE_VSNPRINTF
-
-#ifndef fc_assert
-    #define fc_assert(x) ((void)(0))
-#endif  //\ fc_assert
 
 //+*********************************  **********************************/
 /**
@@ -63,16 +60,95 @@ void fc_log_set_level(fc_log_t *log, fc_log_level_t level)
     log->level = level;
 }
 
+#if (0 != FC_LOG_USE_FC_POOL)
+
+    #define LOG_FILE_LINEAR_WRITE 1
+
+// 传递给write函数的用户数据
+typedef struct _fc_log_file_user_t fc_log_file_user_t;
+struct _fc_log_file_user_t
+{
+    fc_log_t      *log;         // 关联的log对象
+    fc_log_pool_t *pool;        // 第一块内存池
+    void          *start_pool;  // 记录第一块内存地址
+    size_t         write_size;  // 已写入的大小
+
+    #if !(LOG_FILE_LINEAR_WRITE)
+    size_t offset;  // 偏移
+    #endif
+};
+
+/**
+ * @brief 具体查看vfprintf.c中_write_ch函数
+ *
+ *
+ * @param buf
+ * @param len 每次只会写入1字节,结尾会调用一次0长度的写入
+ * @return int
+ */
+static int __fc_log_alloc_write(FC_FILE *f, const void *buf, int len)
+{
+    #if LOG_FILE_LINEAR_WRITE
+
+    // 只会在需要分配内存(FC_IO_SWAP == 0)和结束(FC_IO_EOF == -1)的时候调用
+    if (len >= FC_IO_SWAP)
+    {
+        fc_log_file_user_t *user = (fc_log_file_user_t *)f->user;
+
+        fc_log_t      *log = user->log;
+        fc_log_pool_t *pool = user->pool;
+        user->write_size += pool->size;
+        if (false == log->alloc(FC_LOG_ALLOC_NEW, pool, FC_LOG_LINE_SIZE))
+        {
+            // 结束序列化
+            return -1;  // 这里丢失由内存池分配函数alloc记录
+        }
+
+        // 切换到新的内存块
+        f->p_now = pool->buff;
+        f->p_start = pool->buff;
+        f->p_end = (char *)((uint8_t *)(pool->buff) + pool->size);
+    }
+
+    #else
+
+    // 分配内存(FC_IO_SWAP == 0)和结束(FC_IO_EOF == -1)和写入数据(len > 0,实际每次仅写入1字节)都会调用
+    if (len >= FC_IO_SWAP)
+    {
+        fc_log_file_user_t *user = (fc_log_file_user_t *)f->user;
+        if (user->offset >= user->pool->size)
+        {
+            fc_log_t      *log = user->log;
+            fc_log_pool_t *pool = user->pool;
+            user->write_size += pool->size;
+            if (false == log->alloc(FC_LOG_ALLOC_NEW, pool, FC_LOG_LINE_SIZE))
+            {
+                // 结束序列化
+                return -1;  // 这里丢失由内存池分配函数alloc记录
+            }
+            user->offset = 0;
+        }
+
+        *((char *)(user->pool->buff) + user->offset) = *((char *)buf);  // 直接写入当前内存块
+        user->offset++;
+    }
+
+    #endif
+
+    return len;
+}
+
+#endif  //\ FC_LOG_USE_FC_POOL
+
 /**
  * @brief
  *
  * @param log
  * @param level
- * @param advice_size
  * @param fmt
  * @param ...
  */
-void fc_log_printf(fc_log_t *log, fc_log_level_t level, int advice_size, const char *fmt, ...)
+fc_weak void fc_log_fprintf(fc_log_t *log, fc_log_level_t level, const char *fmt, ...)
 {
     fc_assert(log != NULL);
     fc_assert(log->alloc != NULL);
@@ -81,11 +157,12 @@ void fc_log_printf(fc_log_t *log, fc_log_level_t level, int advice_size, const c
     {
         int           len = 0;
         fc_log_pool_t pool = {0};
-        log->alloc(FC_LOG_ALLOC_NEW, &pool, advice_size);  // 分配内存池
-        if (NULL == pool.buff)
+        if (false == log->alloc(FC_LOG_ALLOC_NEW, &pool, FC_LOG_LINE_SIZE))  // 分配内存池
         {
-            return;  // 这里丢失由内存池分配函数alloc处理
+            return;  // 这里丢失由内存池分配函数alloc记录
         }
+
+#if (0 == FC_LOG_USE_FC_POOL)
 
         {
             va_list vargs;
@@ -94,12 +171,56 @@ void fc_log_printf(fc_log_t *log, fc_log_level_t level, int advice_size, const c
             va_end(vargs);
         }
 
-        len -= log->write(pool.buff, len);
+        if (len > pool.size)
+        {
+            if (false == log->alloc(FC_LOG_ALLOC_REALLOC, &pool, len + 1))  // 重新分配内存池
+            {
+                // 这一次丢失由LOSE_HOOK记录
+                FC_LOG_LOSE_HOOK(false, log, NULL, len - pool.size);
+                len = pool.size;  // 截断
+            }
+            else
+            {
+                va_list vargs;
+                va_start(vargs, fmt);
+                len = LOG_VSNPRINTF(pool.buff, pool.size, fmt, vargs);
+                va_end(vargs);
+            }
+        }
+
+#else
+
+        fc_log_file_user_t user = {0};
+        {
+            user.log = log;
+            user.pool = &pool;
+            user.start_pool = pool.buff;
+
+            FC_FILE f = {0};
+    #if LOG_FILE_LINEAR_WRITE
+            f.p_now = pool.buff;
+            f.p_start = pool.buff;
+            f.p_end = pool.buff + pool.size;
+    #endif
+            f.user = (void *)&user;
+            f.io.write = __fc_log_alloc_write;
+
+            va_list vargs;
+            va_start(vargs, fmt);
+            len = fc_vfprintf(&f, fmt, vargs);
+            va_end(vargs);
+        }
+
+#endif  //\ FC_LOG_USE_FC_POOL
+
+        len -= log->write((void *)&user, user.start_pool, len);  // 写入数据
         FC_LOG_LOSE_HOOK(0 == len, log, pool.buff, len);
         log->alloc(FC_LOG_ALLOC_FREE, &pool, 0);  // 释放内存池
     }
 }
 
+#if 0
+// 废弃
 /**
  * @brief 使用者自己去保证write的线程安全性
  *
@@ -110,7 +231,7 @@ void fc_log_printf(fc_log_t *log, fc_log_level_t level, int advice_size, const c
  * @param fmt
  * @param ...
  */
-void fc_log_printf_stack(fc_log_t *log, fc_log_level_t level, char *stack_buf, int stack_size, const char *fmt, ...)
+void fc_log_fprintf_stack(fc_log_t *log, fc_log_level_t level, char *stack_buf, int stack_size, const char *fmt, ...)
 {
     fc_assert(log != NULL);
     fc_assert(stack_buf != NULL);
@@ -118,7 +239,13 @@ void fc_log_printf_stack(fc_log_t *log, fc_log_level_t level, char *stack_buf, i
 
     if (log->level >= level)
     {
-        int len = 0;
+        int                len = 0;
+        fc_log_file_user_t user = {0};
+        {
+            user.log = log;
+            user.pool = NULL;
+            user.start_pool = NULL;
+        }
 
         {
             va_list vargs;
@@ -127,10 +254,11 @@ void fc_log_printf_stack(fc_log_t *log, fc_log_level_t level, char *stack_buf, i
             va_end(vargs);
         }
 
-        len -= log->write(stack_buf, len);
+        len -= log->write((void *)&user, stack_buf, len);  // 写入数据
         FC_LOG_LOSE_HOOK(0 == len, log, stack_buf, len);
     }
 }
+#endif
 
 #if 0
 // 废弃
@@ -155,58 +283,14 @@ void fc_log_write(fc_log_t *log, fc_log_level_t level, const void *buff, int len
 }
 #endif
 
-//+********************************* 默认自带的写函数 **********************************/
-
-/**
- * @brief
- *
- * @param buf
- * @param len
- * @return int
- */
-fc_weak int log_write_default(const char *buf, int len)
-{
-    // 提供两个默认的write函数,一个是输出到fc_stdout的0号缓冲区,一个是输出到fc_trans的0号虚拟页
-    extern int log_write_stdout(const char *buf, int len);  // 放在fc_port.c中实现,提供一份弱实现
-    extern int log_write_trans(const char *buf, int len);   // 放在fc_trans.c中实现,提供一份弱实现
-
-    return log_write_stdout(buf, len);
-}
-
-//+********************************* 默认自带的内存池分配函数 **********************************/
-
-/**
- * @brief  弱函数建议重写一份,用于调用日志组件的时候分配缓冲区,如果是操作系统可以提供多份固定大小的内存池或者动态内存
- *
- * @param alloc_type
- * @param pool
- * @param advice_size
- * @return int 返回值目前未使用
- */
-fc_weak int log_alloc_default(fc_alloc_type_t alloc_type, fc_log_pool_t *pool, int advice_size)
-{
-    (void)alloc_type;
-    (void)advice_size;
-
-    fc_assert(pool != NULL);
-
-    static char __buff[FC_LOG_LINE_SIZE] = {0};  // 默认内存池大小
-
-    // 默认为单线程且不考虑中断等情况
-    pool->buff = __buff;          // 内存池缓冲区
-    pool->size = sizeof(__buff);  // 内存池大小
-    return sizeof(__buff);
-}
-
 //+********************************* 写入丢失记录 **********************************/
 
-#include "fc_compiler.h"
 /**
  * @brief log写入丢失钩子,弱函数,定义了自己的fc_log对象可以重写
  *  记得在宏FC_LOG_LOSE_HOOK里面去开启,默认是关闭了的
  * @param log
- * @param buff
- * @param len
+ * @param buff 有可能是NULL
+ * @param len buff为NULL时表示内存池分配失败截断丢弃的长度
  * @return int 弱函数,可以在外面重写
  */
 fc_weak int fc_log_write_lose_hook(fc_log_t *log, const void *buff, int len)
@@ -235,3 +319,150 @@ fc_weak int fc_log_write_lose_hook(fc_log_t *log, const void *buff, int len)
 
 // 默认实例化对象
 FC_LOG_IMPL(default_log, FC_LOG_ALL, log_write_default, log_alloc_default);
+
+#if (0 == FC_LOG_USE_FC_POOL)
+
+//+********************************* 默认自带的写函数 **********************************/
+
+/**
+ * @brief
+ *
+ * @param user
+ * @param buf
+ * @param len
+ * @return fc_weak
+ */
+fc_weak int log_write_default(void *user, const char *buf, int len)
+{
+    (void)user;
+    // 提供两个默认的write函数,一个是输出到fc_stdout的0号缓冲区,一个是输出到fc_trans的0号虚拟页
+    extern int log_write_stdout(const char *buf, int len);  // 放在fc_port.c中实现,提供一份弱实现
+    extern int log_write_trans(const char *buf, int len);   // 放在fc_trans.c中实现,提供一份弱实现
+
+    return log_write_stdout(buf, len);
+}
+
+//+********************************* 默认自带的内存池分配函数(无'并行'支持) **********************************/
+
+/**
+ * @brief  弱函数建议重写一份,用于调用日志组件的时候分配缓冲区,如果是操作系统可以提供多份固定大小的内存池或者动态内存
+ *
+ * @param alloc_type
+ * @param pool
+ * @param advice_size
+ * @return bool 返回值表示是否成功分配内存池
+ */
+fc_weak bool log_alloc_default(fc_alloc_type_t alloc_type, fc_log_pool_t *pool, int advice_size)
+{
+    (void)alloc_type;
+    (void)advice_size;
+
+    fc_assert(pool != NULL);
+
+    static char __buff[FC_LOG_LINE_SIZE] = {0};  // 默认内存池大小
+
+    bool ret = false;
+
+    if (FC_LOG_ALLOC_NEW == alloc_type)
+    {
+        // 默认为单线程且不考虑中断等情况
+        pool->buff = __buff;          // 内存池缓冲区
+        pool->size = sizeof(__buff);  // 内存池大小
+
+        ret = true;
+    }
+    else
+    {
+        // 仅仅用于表示有丢失(数量不准确)
+        FC_LOG_LOSE_HOOK(false, FC_LOG_OBJ, &__buff, sizeof(__buff));
+    }
+
+    return ret;
+}
+
+#else
+
+//+********************************* 使用fc_pool进行管理 **********************************/
+
+    #include "fc_auto_init.h"
+    #include "fc_pool.h"
+
+fc_pool_t fc_log_pool;  // log组件使用的内存池
+
+    // log组件使用的内存池总大小(字节),建议向上取整到sizeof(size_t)
+    #ifndef FC_LOG_POOL_TOTAL_SIZE
+        #define FC_LOG_POOL_TOTAL_SIZE (4 * 1024) /* 默认4KB */
+    #endif
+
+    // 每块pool用户可使用大小(字节),内部会向上取整到sizeof(size_t)
+    #ifndef FC_LOG_ALLOC_BLOCK_SIZE
+        #define FC_LOG_ALLOC_BLOCK_SIZE FC_LOG_LINE_SIZE /* 128的话满足大多数情况下日志需求,减少内存分配次数 */
+    #endif
+
+void fc_log_pool_init(void)
+{
+    static size_t log_pool_mem[FC_LOG_POOL_TOTAL_SIZE / sizeof(size_t)];  // 内存池
+    fc_pool_init(&fc_log_pool, log_pool_mem, sizeof(log_pool_mem), FC_LOG_ALLOC_BLOCK_SIZE);
+}
+INIT_EXPORT_ENV(fc_log_pool_init, FC_LOG_INIT_ORDER - 1);  // 确保在log_init之前初始化
+
+/**
+ * @brief
+ *
+ * @param alloc_type
+ * @param pool
+ * @param advice_size
+ * @return fc_weak
+ */
+fc_weak bool log_alloc_default(fc_alloc_type_t alloc_type, fc_log_pool_t *pool, int advice_size)
+{
+    if (FC_LOG_ALLOC_NEW == alloc_type)
+    {
+        void *new_buff = fc_pool_alloc(&fc_log_pool, &(pool->size));
+
+        if (new_buff)
+        {
+            if (pool->buff)  // 不是第一次分配
+            {
+                fc_pool_link(pool->buff, new_buff);  // 链接起来
+            }
+            pool->buff = new_buff;  // 切换到新内存块
+            return true;
+        }
+    }
+    else if (FC_LOG_ALLOC_FREE == alloc_type)
+    {
+        // 交给write的时候push,可以知道实际用到了多少内存
+        // push到used_list尾部
+        // fc_mempool_fifo_push(&g_mempool, pool->buff);
+    }
+
+    return false;
+}
+
+/**
+ * @brief
+ *
+ * @param user
+ * @param buf
+ * @param len
+ * @return fc_weak
+ */
+fc_weak int log_write_default(void *user, const char *buf, int len)
+{
+    if (buf == NULL || len <= 0)
+    {
+        return 0;
+    }
+
+    fc_log_file_user_t *file_user = (fc_log_file_user_t *)user;
+
+    // 标记最后一块已使用大小
+    fc_pool_mark_used((void *)(file_user->pool->buff), len - file_user->write_size);
+
+    fc_pool_fifo_push(&fc_log_pool, (void *)buf);  // 压入fifo
+
+    return len;
+}
+
+#endif  //\ FC_LOG_USE_FC_POOL
--- src/fc_log.h
+++ src/fc_log.h
@@ -42,19 +42,21 @@
         #define FC_LOG_USING_COLOR 1 /**< 是否使用颜色 */
     #endif
 
+    // 宏展开辅助宏
+    #undef __FC_LOG_MACRO_EXPANDING
+    #define __FC_LOG_MACRO_EXPANDING(...) __VA_ARGS__
+
     //! 格式需要转义的数量和格式内容的数量/类型必须匹配
     #ifndef FC_LOG_PREFIX_FMT
         #define FC_LOG_PREFIX_FMT "(%d)%s:" /**< 默认输出时间和当前函数名 */
     #endif
 
     #ifndef FC_LOG_PREFIX_CONTENT
-        #undef __MACRO_EXPANDING
-        #define __MACRO_EXPANDING(...) __VA_ARGS__
-        #define FC_LOG_PREFIX_CONTENT __MACRO_EXPANDING((uint32_t)666, __FUNCTION__)
+        #define FC_LOG_PREFIX_CONTENT __FC_LOG_MACRO_EXPANDING((uint32_t)666, __FUNCTION__)
 
     // #include <stdint.h>
     // extern uint32_t HAL_GetTick(void);
-    //     #define FC_LOG_PREFIX_CONTENT __MACRO_EXPANDING((uint32_t)HAL_GetTick(), __FUNCTION__)
+    //     #define FC_LOG_PREFIX_CONTENT __FC_LOG_MACRO_EXPANDING((uint32_t)HAL_GetTick(), __FUNCTION__)
     #endif
 
     #ifndef FC_LOG_END
@@ -62,12 +64,25 @@
     // #define FC_LOG_END "\r\n"  // 每句log自带换行
     #endif
 
-    #define FC_LOG_MEM_POOL 0   // 使用自定义的内存池配置
-    #define FC_LOG_MEM_STACK 1  // 使用栈缓冲区
-    #define FC_LOG_MEM_NONE 2   // 不使用行缓冲区,直接直接借助自定义printf输出
+    // 不同等级的log前缀
+    #ifndef FC_LOG_ERROR_HEAD
+        #define FC_LOG_ERROR_HEAD "E"
+    #endif
 
-    #ifndef FC_LOG_MEM_TYPE
-        #define FC_LOG_MEM_TYPE FC_LOG_MEM_POOL /**< log缓冲区类型 */
+    #ifndef FC_LOG_WARNING_HEAD
+        #define FC_LOG_WARNING_HEAD "W"
+    #endif
+
+    #ifndef FC_LOG_INFO_HEAD
+        #define FC_LOG_INFO_HEAD "I"
+    #endif
+
+    #ifndef FC_LOG_DEBUG_HEAD
+        #define FC_LOG_DEBUG_HEAD "D"
+    #endif
+
+    #ifndef FC_LOG_VERBOSE_HEAD
+        #define FC_LOG_VERBOSE_HEAD "V"
     #endif
 
 // clang-format off
@@ -100,17 +115,17 @@
      * log级别字符(包含颜色)
      */
     #if FC_LOG_USING_COLOR == 1
-        #define FC_ERROR_TEXT      CSI(31) "E" FC_LOG_PREFIX_FMT CSI(39) FC_LOG_CSI_END    /**< 错误标签 */
-        #define FC_WARNING_TEXT    CSI(33) "W" FC_LOG_PREFIX_FMT CSI(39) FC_LOG_CSI_END    /**< 警告标签 */
-        #define FC_INFO_TEXT       CSI(32) "I" FC_LOG_PREFIX_FMT CSI(39) FC_LOG_CSI_END    /**< 信息标签 */
-        #define FC_DEBUG_TEXT      CSI(34) "D" FC_LOG_PREFIX_FMT CSI(39) FC_LOG_CSI_END    /**< 调试标签 */
-        #define FC_VERBOSE_TEXT    CSI(36) "V" FC_LOG_PREFIX_FMT CSI(39) FC_LOG_CSI_END    /**< 冗余信息标签 */
+        #define FC_ERROR_TEXT      CSI(31) FC_LOG_ERROR_HEAD    FC_LOG_PREFIX_FMT CSI(39) FC_LOG_CSI_END    /**< 错误标签 */
+        #define FC_WARNING_TEXT    CSI(33) FC_LOG_WARNING_HEAD  FC_LOG_PREFIX_FMT CSI(39) FC_LOG_CSI_END    /**< 警告标签 */
+        #define FC_INFO_TEXT       CSI(32) FC_LOG_INFO_HEAD     FC_LOG_PREFIX_FMT CSI(39) FC_LOG_CSI_END    /**< 信息标签 */
+        #define FC_DEBUG_TEXT      CSI(34) FC_LOG_DEBUG_HEAD    FC_LOG_PREFIX_FMT CSI(39) FC_LOG_CSI_END    /**< 调试标签 */
+        #define FC_VERBOSE_TEXT    CSI(36) FC_LOG_VERBOSE_HEAD  FC_LOG_PREFIX_FMT CSI(39) FC_LOG_CSI_END    /**< 冗余信息标签 */
     #else
-        #define FC_ERROR_TEXT      "E" FC_LOG_PREFIX_FMT
-        #define FC_WARNING_TEXT    "W" FC_LOG_PREFIX_FMT
-        #define FC_INFO_TEXT       "I" FC_LOG_PREFIX_FMT
-        #define FC_DEBUG_TEXT      "D" FC_LOG_PREFIX_FMT
-        #define FC_VERBOSE_TEXT    "V" FC_LOG_PREFIX_FMT
+        #define FC_ERROR_TEXT       FC_LOG_ERROR_HEAD       FC_LOG_PREFIX_FMT
+        #define FC_WARNING_TEXT     FC_LOG_WARNING_HEAD     FC_LOG_PREFIX_FMT
+        #define FC_INFO_TEXT        FC_LOG_INFO_HEAD        FC_LOG_PREFIX_FMT
+        #define FC_DEBUG_TEXT       FC_LOG_DEBUG_HEAD       FC_LOG_PREFIX_FMT
+        #define FC_VERBOSE_TEXT     FC_LOG_VERBOSE_HEAD     FC_LOG_PREFIX_FMT
     #endif
 
     typedef enum
@@ -126,23 +141,25 @@
 
 // clang-format on
 
+// 存在多次获取,但是释放仅释放一次
 typedef enum
 {
     FC_LOG_ALLOC_FREE = 0, /**< 释放内存池 */
     FC_LOG_ALLOC_NEW,      /**< 分配新内存池 */
+    FC_LOG_ALLOC_REALLOC,  /**< 重新分配内存池 */
 } fc_alloc_type_t;
 
 //+********************************* 面向对象 **********************************/
 typedef struct _fc_log_t      fc_log_t;
 typedef struct _fc_log_pool_t fc_log_pool_t;
 
-typedef int (*fc_log_write_t)(const char *buf, int len);                                          // 写入数据
-typedef int (*fc_log_alloc_t)(fc_alloc_type_t alloc_type, fc_log_pool_t *pool, int advice_size);  // log内存池处理函数,返回实际分配的内存池大小
+typedef int (*fc_log_write_t)(void *user, const char *buf, int len);                               // 写入数据
+typedef bool (*fc_log_alloc_t)(fc_alloc_type_t alloc_type, fc_log_pool_t *pool, int advice_size);  // log内存池处理函数,返回是否成功
 
 struct _fc_log_pool_t
 {
-    int   size; /**< 内存池大小 */
-    char *buff; /**< 内存池 */
+    size_t size; /**< 内存池大小 */
+    char  *buff; /**< 内存池 */
     // void *user; /**< 用户自定义数据 */
 };
 
@@ -163,11 +180,11 @@ extern "C"
 
     // clang-format off
 
-    extern void fc_log_catch        (fc_log_t *log, fc_log_write_t write, fc_log_alloc_t alloc);
-    extern void fc_log_set_level    (fc_log_t *log, fc_log_level_t level);
-    extern void fc_log_printf       (fc_log_t *log, fc_log_level_t level, int advice_size, const char *fmt, ...);
-    extern void fc_log_printf_stack (fc_log_t *log, fc_log_level_t level, char *stack_buf, int stack_size, const char *fmt, ...);
-    // extern void fc_log_write        (fc_log_t *log, fc_log_level_t level, const void *buff, int len); // 废弃
+    extern void fc_log_catch            (fc_log_t *log, fc_log_write_t write, fc_log_alloc_t alloc);
+    extern void fc_log_set_level        (fc_log_t *log, fc_log_level_t level);
+    extern void fc_log_fprintf          (fc_log_t *log, fc_log_level_t level, const char *fmt, ...);
+    // extern void fc_log_fprintf_stack    (fc_log_t *log, fc_log_level_t level, char *stack_buf, int stack_size, const char *fmt, ...); // 废弃
+    // extern void fc_log_write            (fc_log_t *log, fc_log_level_t level, const void *buff, int len); // 废弃
 
     // clang-format on
 
@@ -175,10 +192,10 @@ extern "C"
     extern int fc_log_write_lose_hook(fc_log_t *log, const void *buff, int len);
 
     // 提供一份默认的log写函数,可以在外面重写
-    extern int log_write_default(const char *buf, int len);
+    extern int log_write_default(void *user, const char *buf, int len);
 
     // 提供一份默认的内存池获取函数,可以在外面重写
-    extern int log_alloc_default(fc_alloc_type_t alloc_type, fc_log_pool_t *pool, int advice_size);  // 默认获取内存池的函数
+    extern bool log_alloc_default(fc_alloc_type_t alloc_type, fc_log_pool_t *pool, int advice_size);
 
     #ifdef __cplusplus
 }
@@ -207,6 +224,24 @@ extern fc_log_t default_log;  // 默认log对象
             }
     #endif
 
+    #ifndef FC_LOG_USE_FC_POOL
+        #include "fc_pool.h"
+        #define FC_LOG_USE_FC_POOL 1 /**< 是否使用fc_poolh及fc_vsnprintf进行格式化 */
+
+    //! 使用fc_pool之后需要在其他地方使用以下方式讲内存池中的数据写出去
+    // void fc_log_walker(bool end, void *ptr, size_t used, void *user)
+    // {
+    //     (void)user;
+    //     (void)end;
+    //     fc_write(ptr, used);
+    // }
+
+    // if (!fc_pool_fifo_empty(&fc_log_pool))
+    // {
+    //     fc_pool_fifo_walk(&fc_log_pool, fc_log_walker, NULL);
+    // }
+    #endif
+
 #endif  // __FC_LOG_H__
 
 //+********************************* 以下部分允许重入 **********************************/
@@ -217,6 +252,7 @@ extern fc_log_t default_log;  // 默认log对象
 #undef fc_log_info
 #undef fc_log_debug
 #undef fc_log_verbose
+#undef fc_log_printf
 #undef fc_log_assert
 
 #undef fc_log_level
@@ -232,48 +268,11 @@ extern fc_log_t default_log;  // 默认log对象
 
 #if FC_LOG_ENABLE
 
-    #if (FC_LOG_MEM_TYPE == FC_LOG_MEM_STACK)
-
-        #if (FC_LOG_STACK_LINE_SIZE < 0)
-            #error "FC_LOG_STACK_LINE_SIZE must be greater than 0"
-            #error "FC_LOG_STACK_LINE_SIZE 必须大于0"
-        #endif
-
-        #define fc_log_format(text, _level, fmt, ...)                                                                                                                         \
-            do                                                                                                                                                                \
-            {                                                                                                                                                                 \
-                char SAFE_NAME(buff)[FC_LOG_STACK_LINE_SIZE]; /* 使用栈内存 */                                                                                                \
-                fc_log_printf_stack(FC_LOG_OBJ, _level, (char *)&(SAFE_NAME(buff)), FC_LOG_STACK_LINE_SIZE, text "" fmt "" FC_LOG_END, FC_LOG_PREFIX_CONTENT, ##__VA_ARGS__); \
-            } while (0)
-
-    #elif (FC_LOG_MEM_TYPE == FC_LOG_MEM_POOL)
-
-        #define fc_log_format(text, _level, fmt, ...)                                                                                 \
-            do                                                                                                                        \
-            {                                                                                                                         \
-                fc_log_printf(FC_LOG_OBJ, _level, FC_LOG_LINE_SIZE, text "" fmt "" FC_LOG_END, FC_LOG_PREFIX_CONTENT, ##__VA_ARGS__); \
-            } while (0)
-
-    #elif (FC_LOG_MEM_TYPE == FC_LOG_MEM_NONE)
-
-        #ifndef FC_LOG_PRINTF
-extern int fc_printf(const char *fmt, ...);
-            #define FC_LOG_PRINTF fc_printf
-        #endif
-
-        #define fc_log_format(text, _level, fmt, ...)                                               \
-            do                                                                                      \
-            {                                                                                       \
-                if (((FC_LOG_OBJ)->level) >= _level)                                                \
-                {                                                                                   \
-                    FC_LOG_PRINTF(text "" fmt "" FC_LOG_END, FC_LOG_PREFIX_CONTENT, ##__VA_ARGS__); \
-                }                                                                                   \
-            } while (0)
-
-    #else
-        #error "FC_LOG_MEM_TYPE unkown"
-        #error "FC_LOG_MEM_TYPE 未知,请检查配置"
-    #endif
+    #define fc_log_format(text, _level, fmt, ...)                                                                \
+        do                                                                                                       \
+        {                                                                                                        \
+            fc_log_fprintf(FC_LOG_OBJ, _level, text "" fmt "" FC_LOG_END, FC_LOG_PREFIX_CONTENT, ##__VA_ARGS__); \
+        } while (0)
 
     //+********************************* 期望使用 **********************************/
     #define fc_log_error(fmt, ...) \
@@ -291,14 +290,15 @@ extern int fc_printf(const char *fmt, ...);
     #define fc_log_verbose(fmt, ...) \
         fc_log_format(FC_VERBOSE_TEXT, FC_LOG_VERBOSE, fmt, ##__VA_ARGS__)
 
-    #ifndef fc_log_assert
-        #define fc_log_assert(expr, ...)                                                                   \
-            if (!(expr))                                                                                   \
-            {                                                                                              \
-                fc_log_error("\"" #expr "\" assert failed at file: %s, line: %d\r\n", __FILE__, __LINE__); \
-                __VA_ARGS__;                                                                               \
-            }
-    #endif
+    #define fc_log_printf(fmt, ...) \
+        fc_log_format("", FC_LOG_NONE, fmt, ##__VA_ARGS__)
+
+    #define fc_log_assert(expr, ...)                                                                   \
+        if (!(expr))                                                                                   \
+        {                                                                                              \
+            fc_log_error("\"" #expr "\" assert failed at file: %s, line: %d\r\n", __FILE__, __LINE__); \
+            __VA_ARGS__;                                                                               \
+        }
 
 #else
 
@@ -311,23 +311,24 @@ extern int fc_printf(const char *fmt, ...);
     #define fc_log_debug    (fmt, ...)              do {} while(0)
     #define fc_log_verbose  (fmt, ...)              do {} while(0)
 
+    #define fc_log_printf   (fmt, ...)              do {} while(0)
 // clang-format on
 
 // 一般在断言中只进行变量比较等操作,通常来说取消断言后效果需要等效完全注释掉
-    #ifndef fc_log_assert
-    // #define fc_log_assert(expr, ...) do {} while(0)
-        #define fc_log_assert(expr, ...) \
-            if (!(expr))                 \
-            {                            \
-                (void)0;                 \
-                __VA_ARGS__;             \
-            }
-    #endif
+    #define fc_log_assert(expr, ...) \
+        if (!(expr))                 \
+        {                            \
+            (void)0;                 \
+            __VA_ARGS__;             \
+        }
+
+// #define fc_log_assert(expr, ...) do {} while(0)
 
 #endif
 
 // clang-format off
 
+// 去掉fc_前缀的log宏API
 #if FC_LOG_NOPREFIX_API
     #define log_level       fc_log_level
     #define log_error       fc_log_error
@@ -335,6 +336,9 @@ extern int fc_printf(const char *fmt, ...);
     #define log_info        fc_log_info
     #define log_debug       fc_log_debug
     #define log_verbose     fc_log_verbose
+
+    #define log_printf      fc_log_printf
+
     #define log_assert      fc_log_assert
 #endif
 
